5c431f4890449c4cdc7ba58e2a45eed57fbcf9d8
代钤莹 <daiqianying@cvte.com>
2022-03-10 10:55:10 +0800

    [modify]更待代码

--- hello_world.c
+++ hello_world.c
@@ -6,12 +6,28 @@
 
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/stdio_usb.h"
+
+#ifndef PICO_DEFAULT_LED_PIN
+#warning blink example requires a board with a regular LED
+#endif
 
 int main() {
     stdio_init_all();
+    stdio_usb_init();
+    
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    
     while (true) {
         printf("Hello, world!\n");
-        sleep_ms(1000);
+        sleep_ms(500);
+        
+        gpio_put(LED_PIN, 1);
+        sleep_ms(250);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(250);
     }
     return 0;
 }

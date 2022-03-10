19d1e48f16b4a92e36f3636007277c3cc9a5ec6a
代钤莹 <daiqianying@cvte.com>
2022-03-10 14:19:43 +0800

    格式化代码

--- hello_world.c
+++ hello_world.c
@@ -12,21 +12,27 @@
 #warning blink example requires a board with a regular LED
 #endif
 
-int main() {
+int main()
+{
     stdio_init_all();
 
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-    
-    while (true) {
+
+    while (true)
+    {
         printf("Hello, world!\n");
         sleep_ms(500);
-        
+
         gpio_put(LED_PIN, 1);
         sleep_ms(250);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
+
+        if (stdio_usb_connected())
+        {
+            gpio_put(LED_PIN, 0);
+            sleep_ms(250);
+        }
     }
     return 0;
 }
